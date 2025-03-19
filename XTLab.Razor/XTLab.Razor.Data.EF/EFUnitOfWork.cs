da73fe5989c07be7a906b5a9935602c8ad8bdb38
Odegaard <tricdt.ars@gmail.com>
2025-03-19 09:39:26 +0700

    [XTLab.Razor] Seeding data

---
+++ XTLab.Razor/XTLab.Razor.Data.EF/EFUnitOfWork.cs
@@ -0,0 +1,22 @@
+using System;
+using XTLab.Razor.Infrastructure.Interfaces;
+
+namespace XTLab.Razor.Data.EF;
+
+public class EFUnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    public EFUnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+    public void Commit()
+    {
+        _context.SaveChanges();
+    }
+
+    public void Dispose()
+    {
+        _context.Dispose();
+    }
+}
