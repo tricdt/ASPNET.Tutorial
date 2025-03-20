edf981929bf995de44cb8d6c395f8139a823f9eb
Odegaard <tricdt.ars@gmail.com>
2025-03-20 14:44:03 +0700

    [XTLab.MvcApp] Create and test firstController

---
+++ XTLab.MvcApp/XTLab.MvcApp.Data/Entities/Product.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace XTLab.MvcApp.Data.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public double Price { get; set; }
+}
---
+++ XTLab.MvcApp/XTLab.MvcApp.Infrastructure/SharedKernel/DomainEntity.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace XTLab.MvcApp.Infrastructure.SharedKernel;
+
+public abstract class DomainEntity<T>
+{
+    public T Id { get; set; }
+
+    /// <summary>
+    /// True if domain entity has an identity
+    /// </summary>
+    /// <returns></returns>
+    public bool IsTransient()
+    {
+        return Id.Equals(default(T));
+    }
+}
