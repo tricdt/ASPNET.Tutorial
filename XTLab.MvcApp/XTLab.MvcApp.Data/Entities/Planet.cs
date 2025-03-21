7fc51def2d1af5c3ff52a88e690ca019c332d899
Odegaard <tricdt.ars@gmail.com>
2025-03-21 15:33:47 +0700

    [XTLab.MvcApp] Routing, Area, UrlHelper, AnchorTagHelper

---
+++ XTLab.MvcApp/XTLab.MvcApp.Data/Entities/Planet.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace XTLab.MvcApp.Data.Entities;
+
+public class Planet
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+
+    public string VnName { get; set; }
+
+    public string Content { get; set; }
+}
