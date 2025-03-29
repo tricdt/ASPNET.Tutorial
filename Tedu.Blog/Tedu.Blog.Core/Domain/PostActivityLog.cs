8ebc2af13c0770537306d8054f91e18902ac272a
BukayoSaka <tricdt@gmail.com>
2025-03-29 15:11:26 +0700

    [Tedu.Blog] Create entities for content

---
+++ Tedu.Blog/Tedu.Blog.Core/Domain/PostActivityLog.cs
@@ -0,0 +1,24 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tedu.Blog.Core.Domain;
+
+[Table("PostActivityLogs")]
+public class PostActivityLog
+{
+    [Key]
+    public Guid Id { get; set; }
+    public Guid PostId { get; set; }
+
+    public PostStatus FromStatus { set; get; }
+
+    public PostStatus ToStatus { set; get; }
+
+    public DateTime DateCreated { get; set; }
+
+    [MaxLength(500)]
+    public string? Note { set; get; }
+
+    public Guid UserId { get; set; }
+}
---
+++ Tedu.Blog/Tedu.Blog.Core/Domain/Tag.cs
@@ -0,0 +1,16 @@
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Tedu.Blog.Core.Domain;
+
+[Table("Tags")]
+public class Tag
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required]
+    [MaxLength(100)]
+    public required string Name { get; set; }
+}
