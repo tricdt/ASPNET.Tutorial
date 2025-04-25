aa8b183feac3ab18235b58f9571b23848c3087b2
BukayoSaka <tricdt@gmail.com>
2025-04-25 15:44:57 +0700

    [Tedu.KnowledgeSpace] Migrate Database

---
+++ Tedu.KnowledgeSpace/Tedu.KnowledgeSpace.BackendServer/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Tedu.KnowledgeSpace.BackendServer.Data;
+
+public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+{
+    public ApplicationDbContext CreateDbContext(string[] args)
+    {
+        var environmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        IConfigurationRoot configuration = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json")
+            .AddJsonFile($"appsettings.{environmentName}.json")
+            .Build();
+        var builder = new DbContextOptionsBuilder<ApplicationDbContext>();
+        var connectionString = configuration.GetConnectionString("DefaultConnection");
+        builder.UseSqlServer(connectionString);
+        return new ApplicationDbContext(builder.Options);
+    }
+}
