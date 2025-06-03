a379bb6468124f54ebaf50fa1534053acc14b323
BukayoSaka <tricdt@gmail.com>
2025-06-03 10:27:27 +0700

    [DDD.TodoApp] Finish add Task

--- DDD.TodoApp/src/DDD.TodoApp/Infrastructure/Mapping/TaskProfile.cs
+++ DDD.TodoApp/src/DDD.TodoApp/Infrastructure/Mapping/TaskProfile.cs
@@ -1,8 +1,13 @@
 using System;
 using AutoMapper;
+using DDD.TodoApp.Commands.Tasks;
+using DDD.TodoApp.ViewModels.Tasks;
 namespace DDD.TodoApp.Infrastructure.Mapping;
 
 public class TaskProfile : Profile
 {
-
+    public TaskProfile()
+    {
+          CreateMap<TasksEditViewModel, TaskAddOrEditCommand>();
+    }
 }
