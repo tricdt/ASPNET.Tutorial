6b95d0accdc13202eef86a26249f3832e1138910
Odegaard <tricdt@gmail.com>
2025-06-05 09:33:56 +0700

    [DDD.AspNetCore] Add health check

--- DDD.AspNetCore/src/DDD.Services.Api/StartupExtensions/HealthCheckExtension.cs
+++ DDD.AspNetCore/src/DDD.Services.Api/StartupExtensions/HealthCheckExtension.cs
@@ -1,15 +1,36 @@
 using System;
-
+using DDD.Infra.Data.Context;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 namespace DDD.Services.Api.StartupExtensions;
 
 public static class HealthCheckExtension
 {
     public static IServiceCollection AddCustomizedHealthCheck(this IServiceCollection services, IConfiguration configuration, IWebHostEnvironment env)
     {
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("DefaultConnection"))
+            .AddDbContextCheck<ApplicationDbContext>();
+
+        services.AddHealthChecksUI(opt =>
+        {
+            opt.SetEvaluationTimeInSeconds(15); // time in seconds between check
+        }).AddInMemoryStorage();
         return services;
     }
 
     public static void UseCustomizedHealthCheck(IEndpointRouteBuilder endpoints, IWebHostEnvironment env)
     {
+        endpoints.MapHealthChecks("/hc", new HealthCheckOptions
+        {
+            Predicate = _ => true,
+            ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse,
+        });
+
+        endpoints.MapHealthChecksUI(setup =>
+        {
+            setup.UIPath = "/hc-ui";
+            setup.ApiPath = "/hc-json";
+        });
     }
 }
