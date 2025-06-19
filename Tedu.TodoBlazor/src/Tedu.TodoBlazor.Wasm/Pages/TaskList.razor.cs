621b45e4b447aa4495d1232f17dddafffdb3ca52
BukayoSaka <tricdt@gmail.com>
2025-06-19 14:31:49 +0700

    [Tedu.TodoBlazor] Component communication

--- Tedu.TodoBlazor/src/Tedu.TodoBlazor.Wasm/Pages/TaskList.razor.cs
+++ Tedu.TodoBlazor/src/Tedu.TodoBlazor.Wasm/Pages/TaskList.razor.cs
@@ -10,22 +10,17 @@ namespace Tedu.TodoBlazor.Wasm.Pages;
 public partial class TaskList
 {
     [Inject] private ITaskApiClient TaskApiClient { set; get; }
-    [Inject] private IUserApiClient UserApiClient { set; get; }
-    [Inject] private IToastService ToastService { set; get; }
+
 
     private List<TaskDto> Tasks;
-    private List<AssigneeDto> Assignees;
     private TaskListSearch TaskListSearch = new TaskListSearch();
-
     protected override async Task OnInitializedAsync()
     {
         Tasks = await TaskApiClient.GetTaskList(TaskListSearch);
-        Assignees = await UserApiClient.GetAssignees();
     }
-
-    private async Task SearchForm(EditContext context)
+    public async Task SearchTask(TaskListSearch taskListSearch)
     {
+        TaskListSearch = taskListSearch;
         Tasks = await TaskApiClient.GetTaskList(TaskListSearch);
-        ToastService.ShowInfo("Search completed");
     }
 }
