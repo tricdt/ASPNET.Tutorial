5516d714ba4d131ad23f4a236c17f438eba587de
odegaard <tricdt@gmail.com>
2025-06-25 09:21:28 +0700

    [Tedu.Exam] Add Infrastructure

---
+++ Tedu.Exam/src/Services/Examination/Examination.Infrastructure/SeedWork/Constants.cs
@@ -0,0 +1,16 @@
+using System;
+
+namespace Examination.Infrastructure.MongoDb.SeedWork;
+
+public class Constants
+{
+    public class Collections
+    {
+        public const string Category = "category";
+        public const string Question = "question";
+        public const string User = "user";
+        public const string Exam = "exam";
+        public const string ExamResult = "exam_result";
+
+    }
+}
---
+++ Tedu.Exam/src/Services/Examination/Examination.Infrastructure/SeedWork/ExamSettings.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace Examination.Infrastructure.MongoDb.SeedWork;
+
+public class ExamSettings
+{
+    public string IdentityUrl { get; set; }
+
+    public DatabaseSettings DatabaseSettings { get; set; }
+}
+
+public class DatabaseSettings
+{
+    public string ConnectionString { set; get; }
+    public string DatabaseName { get; set; }
+
+}
